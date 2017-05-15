aff701b8ea876219e046a1a3da00453625c63a3b
Loek Delahaye <loekdelahaye@gmail.com>
2017-05-15 21:39:29 +0200

    control panel, prefix location(loaded from config.), preference panel toggle, marker filter(dynamic)

--- Main.js
+++ Main.js
@@ -1,84 +1,181 @@
-var viewer = new Cesium.Viewer('cesiumContainer');
+//Renders the Cesium interface on the div with the id cesiumContainer 
+var viewer = new Cesium.Viewer('cesiumContainer', {
+  animation: false,
+  navigationHelpButton: false
+});
 
+var entities = new Set();
+var types = new Set();
+var startTime = new Date(3000, 0, 0, 0, 0, 0, 0);
+var stopTime = new Date(0, 0, 0, 0, 0, 0, 0);
+var viewerEntities = [];
 
-var entities = [];
+$("#cb_toggle_display").change(function () {
+  $("#menu").toggle("display");
+});
 
-function renderMarkers(data) {
-  for (var i = 0; i < data.length; i++) {
-    data.filter(function (i, n) {
-      entities.push(i);
-      viewer.entities.add({
-        name: i.location.title,
-        id: i.location.id,
-        description: "<h1>Type : " +  i.location.type +  "</h1><p>"  + "Description : " +  i.location.description + "</p>",
-        position: Cesium.Cartesian3.fromDegrees(i.location.point[0].lon, i.location.point[0].lat),
-        point: {
-          pixelSize: 5,
-          color: Cesium.Color.RED,
-          outlineColor: Cesium.Color.WHITE,
-          outlineWidth: 2
-        },
-        label: {
-          text: i.location.title,
-          font: '14pt monospace',
-          style: Cesium.LabelStyle.FILL_AND_OUTLINE,
-          outlineWidth: 2,
-          verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
-          pixelOffset: new Cesium.Cartesian2(0, -9)
-        }
-      });
-    });
-  }
-}
+//Initialisation of script on document load
+$(document).ready(function () {
+  console.log("Document ready");
+  $.ajax({
+    url: "https://dev.stefanvlems.nl/mazezoom/dataset/feed.php",
+    dataType: 'json',
+    async: false,
+    success: function (data) {
+      renderMarkers(data);
+      renderFilter(types);
 
-$.getJSON("https://dev.stefanvlems.nl/mazezoom/dataset/feed.php", function (data) {
-  // console.log("data : ", data);
-  renderMarkers(data);
+    }
+  });
+  initConfig();
+  console.log("Entities initialized : ", entities);
+  console.log("Types initialized: ", types);
 });
 
 
-
-//Handle The click event of the map
-var handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
-handler.setInputAction(function (click) {
-  var pickedObject = viewer.scene.pick(click.position);
-  if (Cesium.defined(pickedObject)) {
-    var entityId = pickedObject.id._id;
-    console.log("comparing entityId : ", entityId);
-    console.log("Logging entities : ", entities);
-    if (containsObject(entityId, entities)) {
-      console.log("Found marker!");
-    }
-    // var oldColor = buildingMap.get(entityId).polygon.material.color;
-    // buildingMap.get(entityId).polygon.material.color = pickColor;
-    // selectedEntity.set(entityId, oldColor);
-
-    // var currentLayer = viewer.scene.imageryLayers.get(1);
-    // if (typeof currentLayer !== 'undefined') {
-    //     var info = currentLayer._imageryProvider._tileProvider.getTileCredits(click.position.x, click.position.y, 0);
-    // }
-    // console.log(pickedObject);
-  }
-}, Cesium.ScreenSpaceEventType.LEFT_CLICK);
-
-// Sandcastle.addToggleButton('Fog', scene.fog.enabled, function(checked) {
-//     scene.fog.enabled = checked;
-// });
-
-
-// viewer.zoomTo(wyoming);
-
-// function addDescription(element) {
-//   element.location.description = '\ <img\   width="50%"\   style="float:left; margin: 0 1em 1em 0;"\   src="//cesiumjs.org/images/2015/02-02/Flag_of_Wyoming.svg"/>\ <p>\   Wyoming is a state in the mountain region of the Western \   United States.\ </p>\ <p>\   Wyoming is the 10th most extensive, but the least populous \  and the second least densely populated of the 50 United \   States. The western two thirds of the state is covered mostly \   with the mountain ranges and rangelands in the foothills of \  the eastern Rocky Mountains, while the eastern third of the \   state is high elevation prairie known as the High Plains. \   Cheyenne is the capital and the most populous city in Wyoming, \   with a population estimate of 62,448 in 2013.\ </p>\ <p>\   Source: \   <a style="color: WHITE"\    target="_blank"\    href="http://en.wikipedia.org/wiki/Wyoming">Wikpedia</a>\ </p>'; 
-// }
-
-
-function containsObject(obj, list) {
-  var i;
-  for (i = 0; i < list.length; i++) {
-    if (list[i].location.id === obj) {
-      return true;
-    }
-  }
-  return false;
+function initConfig() {
+  loadJSON('config.json', function (data) {
+    $.each(data, function (key, value) {
+      switch (key) {
+        case 'presets_locations':
+          for (var i = 0; i < data[key].length; i++) {
+            $("#presets_locations").append('<li class="location_preset"><a>' + value[i] + '</a></li>');
+          }
+          (function () {
+            $(".location_preset").click((function (e) {
+              var adress = e.delegateTarget.innerText;
+              var geocoder = new google.maps.Geocoder();
+              geocodeAddress(geocoder, adress);
+              $("#location_presets_toggle_button").html(adress + '<span class="caret" ></span>');
+            }).bind(this));
+          })();
+          break;
+        default:
+          break;
+      }
+    });
+  });
 }
+
+function geocodeAddress(geocoder, value) {
+  geocoder.geocode({
+    'address': value
+  }, function (results, status) {
+    if (status === 'OK') {
+      console.log(results[0].types[0]);
+      var type = results[0].types[0];
+      var lat = results[0].geometry.location.lat();
+      var long = results[0].geometry.location.lng();
+      console.log("Latitude :" + lat + " " + "Longitude : " + long);
+      viewer.camera.flyTo({
+        destination: Cesium.Cartesian3.fromDegrees(long, lat, 15000.0)
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+};
+
+function calibrateTimeline(i) {
+  var context = this;
+  var entityStartTime = i.location.start;
+  var entityEndTime = i.location.end;
+  if (entityStartTime !== null) {
+    entityStartTime = new Date(entityStartTime, 0, 0, 0, 0, 0, 0);
+    if (entityStartTime < context.startTime) {
+      context.startTime = entityStartTime;
+    }
+  }
+  if (entityEndTime !== null) {
+    entityEndTime = new Date(entityEndTime, 0, 0, 0, 0, 0, 0);
+    if (entityEndTime > context.stopTime) {
+      context.stopTime = entityEndTime;
+    }
+  }
+}
+
+function initializeTimeline() {
+  let startTime = new Cesium.JulianDate.fromDate(this.startTime);
+  let stopTime = new Cesium.JulianDate.fromDate(this.stopTime);
+  viewer.clock.startTime = startTime;
+  viewer.clock.stopTime = stopTime;
+  viewer.clock.currentTime = startTime;
+  viewer.timeline.updateFromClock();
+  viewer.timeline.zoomTo(startTime, stopTime);
+}
+
+function renderMarkers(data) {
+  var context = this;
+  for (var x = 0; x < data.length; x++) {
+    var i = data[x];
+    calibrateTimeline(i);
+    if (!i in entities) {
+      console.log("Adding Entitiy to entities : ", i);
+      entities.add(i);
+    }
+    types.add(i.location.type);
+    viewer.entities.add({
+      name: i.location.title,
+      id: i.location.id,
+      type: i.location.type,
+      description: "<h1>Type : " + i.location.type + "</h1><p>" + "Description : " + i.location.description + "</p>",
+      position: Cesium.Cartesian3.fromDegrees(i.location.point[0].lon, i.location.point[0].lat),
+      point: {
+        pixelSize: 5,
+        color: Cesium.Color.RED,
+        outlineColor: Cesium.Color.WHITE,
+        outlineWidth: 2
+      },
+      label: {
+        text: i.location.title,
+        font: '14pt monospace',
+        style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+        outlineWidth: 2,
+        verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+        pixelOffset: new Cesium.Cartesian2(0, -9)
+      }
+    });
+    ;
+  }
+  viewerEntities = viewer.entities._entities._array;
+  initializeTimeline();
+}
+
+function renderFilter(list) {
+  if (list.size != 0) {
+    for (var v of list) {
+      $("#dd_preferences").append('<li class="preference"><a>' + v + '</a></li>');
+    }
+  } else {
+    console.log("list size 0");
+  }
+
+  $(".preference").click((function (e) {
+    var filterType = e.delegateTarget.innerText;
+    filterList(filterType, viewer);
+    $("#preference_toggle_button").html(filterType + '<span class="caret" ></span>');
+  }).bind(this));
+}
+
+function loadJSON(filepath, callback) {
+  $.getJSON(filepath, function (data) {
+    callback(data);
+  })
+};
+
+function filterList(filterType, viewer) {
+  console.log("FilterList called");
+  console.log("FilterType : ", filterType);
+  console.log("Entities : ", viewer.entities._entities._array);
+  console.log("viewer.entities._entites._array.length", viewer.entities._entities._array.length);
+  for (var i = 0; i < viewer.entities._entities._array.length; i++) {
+
+    if (filterType.indexOf(viewer.entities._entities._array[i]._type) <= -1) {
+      viewer.entities._entities._array[i]._show = false;
+    } else {
+      viewer.entities._entities._array[i]._show = true;
+      console.log("Kept Entity of type : ", viewer.entities._entities._array[i]._type);
+    }
+  }
+}
+
